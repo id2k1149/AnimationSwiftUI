da8e150e0e5795e2de25f7a5f767ccb9eec62c2a
id2k1149 <id2k1149@gmail.com>
2023-05-06 12:57:44 -0400

    Initial Commit

---
+++ AnimationSwiftUI/AnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUIApp.swift
+//  AnimationSwiftUI
+//
+//  Created by Max Franz Immelmann on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
